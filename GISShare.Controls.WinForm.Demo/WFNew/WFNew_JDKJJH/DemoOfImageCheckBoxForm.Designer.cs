fc89e128f1554fbe4ae79bc9e99598f4f46a56d9
gisshare2015 <441856317@qq.com>
2024-02-22 18:09:05 +0800

    修改Demo
    修改Demo

--- GISShare.Controls.WinForm.Demo/WFNew/WFNew_JDKJJH/DemoOfImageCheckBoxForm.Designer.cs
+++ GISShare.Controls.WinForm.Demo/WFNew/WFNew_JDKJJH/DemoOfImageCheckBoxForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DemoOfImageCheckBoxForm));
             this.imageCheckBox1 = new GISShare.Controls.WinForm.WFNew.ImageCheckBoxItem();
             this.imageCheckBox2 = new GISShare.Controls.WinForm.WFNew.ImageCheckBoxItem();
             this.imageCheckBox3 = new GISShare.Controls.WinForm.WFNew.ImageCheckBoxItem();
@@ -44,76 +45,103 @@
             // 
             // imageCheckBox1
             // 
+            this.imageCheckBox1.BackgroundColor = System.Drawing.Color.Transparent;
+            this.imageCheckBox1.BackgroundImage = null;
             this.imageCheckBox1.Font = new System.Drawing.Font("宋体", 9F);
             this.imageCheckBox1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.imageCheckBox1.Image = null;
             this.imageCheckBox1.Location = new System.Drawing.Point(0, 0);
             this.imageCheckBox1.Name = "imageCheckBox1";
-            this.imageCheckBox1.Size = new System.Drawing.Size(284, 54);
+            this.imageCheckBox1.OutLineColor = System.Drawing.Color.Transparent;
+            this.imageCheckBox1.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.imageCheckBox1.Size = new System.Drawing.Size(252, 45);
             this.imageCheckBox1.Tag = null;
             this.imageCheckBox1.Text = "图片复选框控件";
             this.imageCheckBox1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // imageCheckBox2
             // 
+            this.imageCheckBox2.BackgroundColor = System.Drawing.Color.Transparent;
+            this.imageCheckBox2.BackgroundImage = null;
             this.imageCheckBox2.Font = new System.Drawing.Font("宋体", 9F);
             this.imageCheckBox2.ForeColor = System.Drawing.SystemColors.ControlText;
             this.imageCheckBox2.Image = null;
             this.imageCheckBox2.Location = new System.Drawing.Point(0, 0);
             this.imageCheckBox2.Name = "imageCheckBox2";
-            this.imageCheckBox2.Size = new System.Drawing.Size(284, 54);
+            this.imageCheckBox2.OutLineColor = System.Drawing.Color.Transparent;
+            this.imageCheckBox2.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.imageCheckBox2.Size = new System.Drawing.Size(252, 45);
             this.imageCheckBox2.Tag = null;
             this.imageCheckBox2.Text = "图片复选框控件";
             this.imageCheckBox2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // imageCheckBox3
             // 
+            this.imageCheckBox3.BackgroundColor = System.Drawing.Color.Transparent;
+            this.imageCheckBox3.BackgroundImage = null;
             this.imageCheckBox3.Font = new System.Drawing.Font("宋体", 9F);
             this.imageCheckBox3.ForeColor = System.Drawing.SystemColors.ControlText;
             this.imageCheckBox3.Image = null;
+            this.imageCheckBox3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.imageCheckBox3.Location = new System.Drawing.Point(0, 0);
             this.imageCheckBox3.Name = "imageCheckBox3";
-            this.imageCheckBox3.Size = new System.Drawing.Size(284, 54);
+            this.imageCheckBox3.OutLineColor = System.Drawing.Color.Transparent;
+            this.imageCheckBox3.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.imageCheckBox3.Size = new System.Drawing.Size(252, 45);
             this.imageCheckBox3.Tag = null;
             this.imageCheckBox3.Text = "图片复选框控件";
+            this.imageCheckBox3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // imageCheckBox4
             // 
+            this.imageCheckBox4.BackgroundColor = System.Drawing.Color.Transparent;
+            this.imageCheckBox4.BackgroundImage = null;
             this.imageCheckBox4.CDSpace = 3;
             this.imageCheckBox4.Font = new System.Drawing.Font("宋体", 9F);
             this.imageCheckBox4.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.imageCheckBox4.Image = null;
+            this.imageCheckBox4.Image = ((System.Drawing.Image)(resources.GetObject("imageCheckBox4.Image")));
             this.imageCheckBox4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.imageCheckBox4.Location = new System.Drawing.Point(0, 0);
             this.imageCheckBox4.Name = "imageCheckBox4";
-            this.imageCheckBox4.Size = new System.Drawing.Size(284, 54);
+            this.imageCheckBox4.OutLineColor = System.Drawing.Color.Transparent;
+            this.imageCheckBox4.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.imageCheckBox4.Size = new System.Drawing.Size(252, 45);
             this.imageCheckBox4.Tag = null;
             this.imageCheckBox4.Text = "图片复选框控件";
+            this.imageCheckBox4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // imageCheckBox5
             // 
+            this.imageCheckBox5.BackgroundColor = System.Drawing.Color.Transparent;
+            this.imageCheckBox5.BackgroundImage = null;
             this.imageCheckBox5.CDSpace = 3;
             this.imageCheckBox5.Font = new System.Drawing.Font("宋体", 9F);
             this.imageCheckBox5.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.imageCheckBox5.Image = null;
+            this.imageCheckBox5.Image = ((System.Drawing.Image)(resources.GetObject("imageCheckBox5.Image")));
             this.imageCheckBox5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.imageCheckBox5.Location = new System.Drawing.Point(0, 0);
             this.imageCheckBox5.Name = "imageCheckBox5";
-            this.imageCheckBox5.Size = new System.Drawing.Size(284, 54);
+            this.imageCheckBox5.OutLineColor = System.Drawing.Color.Transparent;
+            this.imageCheckBox5.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.imageCheckBox5.Size = new System.Drawing.Size(252, 45);
             this.imageCheckBox5.Tag = null;
             this.imageCheckBox5.Text = "图片复选框控件";
             this.imageCheckBox5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // imageCheckBox6
             // 
+            this.imageCheckBox6.BackgroundColor = System.Drawing.Color.Transparent;
+            this.imageCheckBox6.BackgroundImage = null;
             this.imageCheckBox6.CDSpace = 3;
             this.imageCheckBox6.Font = new System.Drawing.Font("宋体", 9F);
             this.imageCheckBox6.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.imageCheckBox6.Image = null;
+            this.imageCheckBox6.Image = ((System.Drawing.Image)(resources.GetObject("imageCheckBox6.Image")));
             this.imageCheckBox6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.imageCheckBox6.Location = new System.Drawing.Point(0, 0);
             this.imageCheckBox6.Name = "imageCheckBox6";
-            this.imageCheckBox6.Size = new System.Drawing.Size(284, 54);
+            this.imageCheckBox6.OutLineColor = System.Drawing.Color.Transparent;
+            this.imageCheckBox6.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.imageCheckBox6.Size = new System.Drawing.Size(252, 45);
             this.imageCheckBox6.Tag = null;
             this.imageCheckBox6.Text = "图片复选框控件";
             this.imageCheckBox6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -121,68 +149,75 @@
             // baseItemHost1
             // 
             this.baseItemHost1.BaseItemObject = this.imageCheckBox1;
-            this.baseItemHost1.Location = new System.Drawing.Point(58, 114);
-            this.baseItemHost1.Margin = new System.Windows.Forms.Padding(9);
+            this.baseItemHost1.Location = new System.Drawing.Point(52, 95);
+            this.baseItemHost1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.baseItemHost1.Name = "baseItemHost1";
             this.baseItemHost1.Padding = new System.Windows.Forms.Padding(0);
-            this.baseItemHost1.Size = new System.Drawing.Size(284, 54);
+            this.baseItemHost1.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.baseItemHost1.Size = new System.Drawing.Size(252, 45);
             this.baseItemHost1.TabIndex = 3;
             // 
             // baseItemHost2
             // 
             this.baseItemHost2.BaseItemObject = this.imageCheckBox2;
-            this.baseItemHost2.Location = new System.Drawing.Point(58, 40);
-            this.baseItemHost2.Margin = new System.Windows.Forms.Padding(9);
+            this.baseItemHost2.Location = new System.Drawing.Point(52, 33);
+            this.baseItemHost2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.baseItemHost2.Name = "baseItemHost2";
             this.baseItemHost2.Padding = new System.Windows.Forms.Padding(0);
-            this.baseItemHost2.Size = new System.Drawing.Size(284, 54);
+            this.baseItemHost2.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.baseItemHost2.Size = new System.Drawing.Size(252, 45);
             this.baseItemHost2.TabIndex = 4;
             // 
             // baseItemHost3
             // 
             this.baseItemHost3.BaseItemObject = this.imageCheckBox3;
-            this.baseItemHost3.Location = new System.Drawing.Point(58, 189);
-            this.baseItemHost3.Margin = new System.Windows.Forms.Padding(9);
+            this.baseItemHost3.Location = new System.Drawing.Point(52, 157);
+            this.baseItemHost3.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.baseItemHost3.Name = "baseItemHost3";
             this.baseItemHost3.Padding = new System.Windows.Forms.Padding(0);
-            this.baseItemHost3.Size = new System.Drawing.Size(284, 54);
+            this.baseItemHost3.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.baseItemHost3.Size = new System.Drawing.Size(252, 45);
             this.baseItemHost3.TabIndex = 5;
             // 
             // baseItemHost4
             // 
             this.baseItemHost4.BaseItemObject = this.imageCheckBox4;
-            this.baseItemHost4.Location = new System.Drawing.Point(360, 189);
-            this.baseItemHost4.Margin = new System.Windows.Forms.Padding(9);
+            this.baseItemHost4.Location = new System.Drawing.Point(320, 157);
+            this.baseItemHost4.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.baseItemHost4.Name = "baseItemHost4";
             this.baseItemHost4.Padding = new System.Windows.Forms.Padding(0);
-            this.baseItemHost4.Size = new System.Drawing.Size(284, 54);
+            this.baseItemHost4.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.baseItemHost4.Size = new System.Drawing.Size(252, 45);
             this.baseItemHost4.TabIndex = 8;
             // 
             // baseItemHost5
             // 
             this.baseItemHost5.BaseItemObject = this.imageCheckBox5;
-            this.baseItemHost5.Location = new System.Drawing.Point(360, 40);
-            this.baseItemHost5.Margin = new System.Windows.Forms.Padding(9);
+            this.baseItemHost5.Location = new System.Drawing.Point(320, 33);
+            this.baseItemHost5.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.baseItemHost5.Name = "baseItemHost5";
             this.baseItemHost5.Padding = new System.Windows.Forms.Padding(0);
-            this.baseItemHost5.Size = new System.Drawing.Size(284, 54);
+            this.baseItemHost5.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.baseItemHost5.Size = new System.Drawing.Size(252, 45);
             this.baseItemHost5.TabIndex = 7;
             // 
             // baseItemHost6
             // 
             this.baseItemHost6.BaseItemObject = this.imageCheckBox6;
-            this.baseItemHost6.Location = new System.Drawing.Point(360, 114);
-            this.baseItemHost6.Margin = new System.Windows.Forms.Padding(9);
+            this.baseItemHost6.Location = new System.Drawing.Point(320, 95);
+            this.baseItemHost6.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.baseItemHost6.Name = "baseItemHost6";
             this.baseItemHost6.Padding = new System.Windows.Forms.Padding(0);
-            this.baseItemHost6.Size = new System.Drawing.Size(284, 54);
+            this.baseItemHost6.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.baseItemHost6.Size = new System.Drawing.Size(252, 45);
             this.baseItemHost6.TabIndex = 6;
             // 
             // DemoOfImageCheckBoxForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(676, 348);
+            this.CancelItemsEventNC = false;
+            this.ClientSize = new System.Drawing.Size(601, 282);
             this.Controls.Add(this.baseItemHost6);
             this.Controls.Add(this.baseItemHost5);
             this.Controls.Add(this.baseItemHost4);
@@ -190,7 +225,7 @@
             this.Controls.Add(this.baseItemHost2);
             this.Controls.Add(this.baseItemHost1);
             this.Location = new System.Drawing.Point(0, 0);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "DemoOfImageCheckBoxForm";
             this.Text = "ImageCheckBox控件";
             this.ResumeLayout(false);
