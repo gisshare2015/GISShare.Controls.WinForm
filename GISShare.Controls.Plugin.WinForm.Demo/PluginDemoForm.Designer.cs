5338dac8de481777931a6f04440d481e11633227
gisshare2015 <441856317@qq.com>
2024-02-22 11:27:42 +0800

    常规维护
    常规维护

--- GISShare.Controls.Plugin.WinForm.Demo/PluginDemoForm.Designer.cs
+++ GISShare.Controls.Plugin.WinForm.Demo/PluginDemoForm.Designer.cs
@@ -39,6 +39,8 @@
             // 
             // dbRibbonHostForm
             // 
+            this.dbRibbonHostForm.BackgroundColor = System.Drawing.Color.Transparent;
+            this.dbRibbonHostForm.BackgroundImage = null;
             this.dbRibbonHostForm.Description = "基于RibbonControl、RibbonStatus、ContextPopupManager和DockPanelManager控件解析的插件应用程序Demo（" +
     "这些控件/组件成员是可选的）";
             this.dbRibbonHostForm.DescriptionFont = new System.Drawing.Font("宋体", 9F);
@@ -49,9 +51,11 @@
             this.dbRibbonHostForm.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.dbRibbonHostForm.Location = new System.Drawing.Point(0, 0);
             this.dbRibbonHostForm.Name = "dbRibbonHostForm";
+            this.dbRibbonHostForm.OutLineColor = System.Drawing.Color.Transparent;
             this.dbRibbonHostForm.Padding = new System.Windows.Forms.Padding(3);
+            this.dbRibbonHostForm.ShadowColor = System.Drawing.SystemColors.ControlText;
             this.dbRibbonHostForm.ShowNomalState = true;
-            this.dbRibbonHostForm.Size = new System.Drawing.Size(508, 114);
+            this.dbRibbonHostForm.Size = new System.Drawing.Size(452, 95);
             this.dbRibbonHostForm.Tag = null;
             this.dbRibbonHostForm.Text = "RibbonHostForm";
             this.dbRibbonHostForm.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -59,6 +63,8 @@
             // 
             // dbDockBarHostTBForm
             // 
+            this.dbDockBarHostTBForm.BackgroundColor = System.Drawing.Color.Transparent;
+            this.dbDockBarHostTBForm.BackgroundImage = null;
             this.dbDockBarHostTBForm.Description = "基于TBForm、DockBarManager和DockPanelManager控件解析的插件应用程序Demo（这些窗体/组件成员是可选的）";
             this.dbDockBarHostTBForm.DescriptionFont = new System.Drawing.Font("宋体", 9F);
             this.dbDockBarHostTBForm.DescriptionForeColor = System.Drawing.SystemColors.ControlText;
@@ -68,9 +74,11 @@
             this.dbDockBarHostTBForm.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.dbDockBarHostTBForm.Location = new System.Drawing.Point(0, 0);
             this.dbDockBarHostTBForm.Name = "dbDockBarHostTBForm";
+            this.dbDockBarHostTBForm.OutLineColor = System.Drawing.Color.Transparent;
             this.dbDockBarHostTBForm.Padding = new System.Windows.Forms.Padding(3);
+            this.dbDockBarHostTBForm.ShadowColor = System.Drawing.SystemColors.ControlText;
             this.dbDockBarHostTBForm.ShowNomalState = true;
-            this.dbDockBarHostTBForm.Size = new System.Drawing.Size(508, 106);
+            this.dbDockBarHostTBForm.Size = new System.Drawing.Size(452, 88);
             this.dbDockBarHostTBForm.Tag = null;
             this.dbDockBarHostTBForm.Text = "DockBarHostTBForm";
             this.dbDockBarHostTBForm.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -78,6 +86,8 @@
             // 
             // dbDockBarHostForm
             // 
+            this.dbDockBarHostForm.BackgroundColor = System.Drawing.Color.Transparent;
+            this.dbDockBarHostForm.BackgroundImage = null;
             this.dbDockBarHostForm.Description = "基于DockBarManager和DockPanelManager控件解析的插件应用程序Demo（这些组件成员是可选的）";
             this.dbDockBarHostForm.DescriptionFont = new System.Drawing.Font("宋体", 9F);
             this.dbDockBarHostForm.DescriptionForeColor = System.Drawing.SystemColors.ControlText;
@@ -87,9 +97,11 @@
             this.dbDockBarHostForm.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.dbDockBarHostForm.Location = new System.Drawing.Point(0, 0);
             this.dbDockBarHostForm.Name = "dbDockBarHostForm";
+            this.dbDockBarHostForm.OutLineColor = System.Drawing.Color.Transparent;
             this.dbDockBarHostForm.Padding = new System.Windows.Forms.Padding(3);
+            this.dbDockBarHostForm.ShadowColor = System.Drawing.SystemColors.ControlText;
             this.dbDockBarHostForm.ShowNomalState = true;
-            this.dbDockBarHostForm.Size = new System.Drawing.Size(508, 106);
+            this.dbDockBarHostForm.Size = new System.Drawing.Size(452, 88);
             this.dbDockBarHostForm.Tag = null;
             this.dbDockBarHostForm.Text = "DockBarHostForm";
             this.dbDockBarHostForm.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -99,50 +111,52 @@
             // 
             this.baseItemHost1.BackColor = System.Drawing.Color.Transparent;
             this.baseItemHost1.BaseItemObject = this.dbRibbonHostForm;
-            this.baseItemHost1.Location = new System.Drawing.Point(18, 18);
-            this.baseItemHost1.Margin = new System.Windows.Forms.Padding(4);
+            this.baseItemHost1.Location = new System.Drawing.Point(16, 15);
+            this.baseItemHost1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.baseItemHost1.Name = "baseItemHost1";
             this.baseItemHost1.Padding = new System.Windows.Forms.Padding(0);
-            this.baseItemHost1.Size = new System.Drawing.Size(508, 114);
+            this.baseItemHost1.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.baseItemHost1.Size = new System.Drawing.Size(452, 95);
             this.baseItemHost1.TabIndex = 1;
             // 
             // baseItemHost2
             // 
             this.baseItemHost2.BackColor = System.Drawing.Color.Transparent;
             this.baseItemHost2.BaseItemObject = this.dbDockBarHostTBForm;
-            this.baseItemHost2.Location = new System.Drawing.Point(18, 256);
-            this.baseItemHost2.Margin = new System.Windows.Forms.Padding(4);
+            this.baseItemHost2.Location = new System.Drawing.Point(16, 213);
+            this.baseItemHost2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.baseItemHost2.Name = "baseItemHost2";
             this.baseItemHost2.Padding = new System.Windows.Forms.Padding(0);
-            this.baseItemHost2.Size = new System.Drawing.Size(508, 106);
+            this.baseItemHost2.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.baseItemHost2.Size = new System.Drawing.Size(452, 88);
             this.baseItemHost2.TabIndex = 2;
             // 
             // baseItemHost3
             // 
             this.baseItemHost3.BackColor = System.Drawing.Color.Transparent;
             this.baseItemHost3.BaseItemObject = this.dbDockBarHostForm;
-            this.baseItemHost3.Location = new System.Drawing.Point(18, 141);
-            this.baseItemHost3.Margin = new System.Windows.Forms.Padding(4);
+            this.baseItemHost3.Location = new System.Drawing.Point(16, 117);
+            this.baseItemHost3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.baseItemHost3.Name = "baseItemHost3";
             this.baseItemHost3.Padding = new System.Windows.Forms.Padding(0);
-            this.baseItemHost3.Size = new System.Drawing.Size(508, 106);
+            this.baseItemHost3.ShadowColor = System.Drawing.SystemColors.ControlText;
+            this.baseItemHost3.Size = new System.Drawing.Size(452, 88);
             this.baseItemHost3.TabIndex = 2;
             // 
             // PluginDemoForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelItemsEventNC = false;
-            this.ClientSize = new System.Drawing.Size(539, 372);
+            this.ClientSize = new System.Drawing.Size(482, 313);
             this.Controls.Add(this.baseItemHost3);
             this.Controls.Add(this.baseItemHost2);
             this.Controls.Add(this.baseItemHost1);
-            this.eRenderStyle = GISShare.Controls.WinForm.WFNew.RenderStyle.eSystem;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMiddleCaptionText = true;
             this.Location = new System.Drawing.Point(0, 0);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "PluginDemoForm";
